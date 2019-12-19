4d99258446ab2095eb212570c68e3bc568cbbf2f
rainy-me <github@rainy.me>
2019-12-19 19:48:27 +0900

    add more shortcuts

--- shortcut/shortcut.go
+++ shortcut/shortcut.go
@@ -6,12 +6,16 @@ var Map = map[string][]string{
 	"b": {"branch"},
 	"c": {"checkout"},
 	"m": {"merge"},
+	"s": {"status"},
 
 	"om":  {"origin", "master"},
 	"cm":  {"commit", "-m"},
 	"rao": {"remote", "add", "origin"},
+	"rgo": {"remote", "get-url", "origin"},
+	"rso": {"remote", "set-url", "origin"},
 	"po":  {"push", "origin"},
 	"pom": {"push", "origin", "master"},
+	"pox": {"push", "origin", "$(git branch --show-current)"},
 	"cb":  {"checkout", "-b"},
 	"db":  {"branch", "-d"},
 }
